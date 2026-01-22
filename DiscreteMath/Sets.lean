8adf6027e00121b5323651f51a48c4736619a452
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2026-01-22 10:06:26 -0900

    some exercises

--- DiscreteMath/Sets.lean
+++ DiscreteMath/Sets.lean
@@ -14,4 +14,9 @@ variable {α : Type*} [DecidableEq α] (a : α) (s t : Finset α)
 
 end
 
+def mul_inbetween (n m k : ℤ) : ℤ :=
+  ⌊n/k⌋ - ⌊(m-1)/k⌋ 
 
+#eval mul_inbetween 100000 10000 10
+#eval mul_inbetween 3400 (-600) 10
+#eval mul_inbetween 73 -73 2
