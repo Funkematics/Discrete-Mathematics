82595adb2d955ae95ef4ae5e12c287c6b8332614
Christopher Munoz <funkycosmonaut@protonmail.com>
2026-01-24 21:37:47 -0900

    more results on sets

--- DiscreteMath/Sets.lean
+++ DiscreteMath/Sets.lean
@@ -14,6 +14,8 @@ variable {α : Type*} [DecidableEq α] (a : α) (s t : Finset α)
 
 end
 
+--The function finds cardinalty of a subset defined by divisibility by k of a larger integer subset of range n to k, ie it finds the number of elements divisibly by k in range [n,k]--
+
 def mul_inbetween (n m k : ℤ) : ℤ :=
   ⌊n/k⌋ - ⌊(m-1)/k⌋ 
 
@@ -22,6 +24,8 @@ def mul_inbetween (n m k : ℤ) : ℤ :=
 variable {α : Type*}
 variable (s t u : Set α)
 open Set
+
+-- Below are 4 ways of proving the exact same theorem --
 
 theorem subsetcap (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
   rw [subset_def]
@@ -37,6 +41,14 @@ theorem subsetcap2 (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
   rintro x ⟨xs, xu⟩
   exact ⟨h _ xs, xu⟩ 
 
+theorem subsetcap3 (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  intro x xsu
+  exact ⟨h xsu.1, xsu.2⟩ 
+
+theorem subsetcap4 (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := 
+  fun x ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
+
+
 theorem subsetcup (h : s ⊆ t) : s ∪ u ⊆ t ∪ u := by
   rw [subset_def]
   rw [union_def]
@@ -48,5 +60,16 @@ theorem subsetcup (h : s ⊆ t) : s ∪ u ⊆ t ∪ u := by
     | inl xs => left; exact h _ xs
     | inr xu => right; exact xu
 
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  intro x hx
+  have xs : x ∈ s := hx.1
+  have xtu : x ∈ t ∪ u := hx.2  --hx.1 and hx.2 ≃ hx.left and hx.right
+  rcases xtu with xt | xu
+  · left
+    show x ∈ s ∩ t
+    exact ⟨xs, xt⟩
+  · right
+    show x ∈ s ∩ u
+    exact ⟨xs, xu⟩ 
 
 
